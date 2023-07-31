7aa5a4dc7504060809d2a4bffe22aad7c3affc75
Denis <92470685+denis02107@users.noreply.github.com>
2023-07-31 08:46:53 +0300

    Add files via upload

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.urls import re_path
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
+urlpatterns += [
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed')
+]
